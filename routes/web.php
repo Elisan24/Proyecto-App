5bf7ae65c4863ff36370a38dd858e77855039f41
CattaHerrera <josecatalino13@gmai.com>
2022-10-16 16:14:51 -0600

    todas las rutas

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,19 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PropertyController;
-
-
-Route::get('Property', 'App\Http\Controllers\PropertyController@index');
-
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\GuestController;
+use App\Http\Controllers\OwnerController;
+use App\Http\Controllers\RestrictionController;
+use App\Http\Controllers\ServiceController;
 
 Route::get('/', function () { return view('welcome');});
+Route::get('Property', 'App\Http\Controllers\PropertyController@index');
+Route::get('Property', 'App\Http\Controllers\CommentController@index');
+Route::get('Property', 'App\Http\Controllers\GuestController@index');
+Route::get('Property', 'App\Http\Controllers\OwnerController@index');
+Route::get('Property', 'App\Http\Controllers\RestrictionController@index');
+Route::get('Property', 'App\Http\Controllers\ServiceController@index');
+
+
+
