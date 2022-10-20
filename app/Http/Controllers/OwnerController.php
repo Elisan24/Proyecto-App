cb3e5b089782c45a70eeb1a5c771fb71ab13f094
CattaHerrera <josecatalino13@gmai.com>
2022-10-20 13:08:49 -0600

    Example

--- app/Http/Controllers/OwnerController.php
+++ app/Http/Controllers/OwnerController.php
@@ -6,5 +6,12 @@ use Illuminate\Http\Request;
 
 class LeaseController extends Controller
 {
-    //
+    public function index(){
+        return "hola mundo";
+    }
+
+    public function create(){
+        return view('welcome');
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,9 @@ use App\Http\Controllers\RestrictionController;
 use App\Http\Controllers\ServiceController;
 
 Route::get('/', function () { return view('welcome');});
-Route::get('Property', 'App\Http\Controllers\PropertyController@index');
+
+
+Route::get('/property', [PropertyController::class, 'index'])->name('home');
 Route::get('Property', 'App\Http\Controllers\CommentController@index');
 Route::get('Property', 'App\Http\Controllers\GuestController@index');
 Route::get('Property', 'App\Http\Controllers\OwnerController@index');
